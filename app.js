3232811c9b22b988b22824533016fd83cd27e003
Luis Jimenez <luchit000@gmail.com>
2014-06-22 22:21:07 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+  var parseId="02xomsnBcLTlxDNSNpZssI6KqVZp10ym1Pru2dhL";
+  var parseRestKey="NNNb2GQnsy2N1Ilmq46Y0H0M0luGNB1wRtg277VM";
+$(document).ready(function(){
+   getMessages(); 
+   $("#send").click(function(){
+      var username = $('input[name=username]').attr('value');
+      var message = $('input[name=message]').attr('value');
+	 console.log(username)
+	 console.log('!')
+	 $.ajax({
+	    url: 'https://api.parse.com/1/classes/MessageBoard',
+	       headers: {
+		  'X-Parse-Application-ID': parseId,
+		  'X-Parse-REST-API-Key': parseRestKey
+	       },
+	       contentType: 'application/json',
+	       dataType: 'json',
+	       processData: false,
+	       data: JSON.stringify){
+		  'username': username,
+		  'message': message
+		  }),
+	       type: 'POST',
+	       success: function() {
+		  console.log('sent');
+		  getMessages();
+	       },
+	       error: funtion(){
+		  console.log('error');
+	       }
+	      });
+	    });
+	 })
+      function getMessages() {
+      $.ajax({
+	 url: 'https://api.parse.com/1/classes/MessageBoard',
+	    headers: {
+	       'X-Parse-Application-Id': parseId,
+	       'X-Parse-REST-API-Key': parseRestKey
+	       },
+	       contentType: 'application/json',
+	       dataType: 'json',
+	       type: 'GET',
+	       success: function(data) {
+		  console.log('get');
+		  updateView(data);
+	       },
+	       error: function() {
+		  console.log('error');
+	       }  
+	    });
+	 }
+      function updateView(messages) {
+	 var = table=$('.table tbody');
+	 table.html('');
+	 $.each(messages.results, function(index, value){
+	    var trE1 = 
+	       $('<tr><td>'
+		  + value.username
+		  + '</td></tr>'
+		  + value.message +
+		  '</td></tr>');
+	    table.append(trE1);
+	 });
+	 console.log(messages);
+      }
+
+
+  test.save({
+    name: "John",
+    text: "hi"}, {
+    success: function(object){
+      console.log("Parse.com object is saved: "+object);
+  },
+  error: function(object) {
+    console.log("Error! Parse.com object is not saved: "+object);
+    }
+  });
+})
+  
